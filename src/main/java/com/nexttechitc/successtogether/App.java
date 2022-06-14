856c8bbb47e5fc6319b064fc572d2493eb4b208d
muna37 <maimunah68@yahoo.com>
2022-06-14 11:56:07 -0400

    practice

---
+++ src/main/java/com/nexttechitc/successtogether/App.java
@@ -0,0 +1,13 @@
+package com.nexttechitc.successtogether;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
